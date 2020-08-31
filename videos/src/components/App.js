3920a803386aac9634d8582d553ab2caf95be53b
Mustafizur <mustafizur8888@gmail.com>
2020-08-31 23:45:00 +0600

    axios and youtube api

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import youtube from '../apis/youtube';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+  state = { videos: [] };
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+      },
+    });
+
+    this.setState({ videos: response.data.items });
+  };
+
   render() {
-    return <SearchBar />;
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+      </div>
+    );
   }
 }
 export default App;
